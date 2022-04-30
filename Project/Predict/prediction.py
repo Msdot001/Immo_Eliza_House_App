70744f6c2e0ec85c2dd1445c48fd2d9940b44956
Moshood Owolabi <98819100+Msdot001@users.noreply.github.com>
2022-04-30 14:51:44 +0200

    model development

---
+++ Project/Predict/prediction.py
@@ -0,0 +1,4 @@
+# should contain all the code used to predict a new house's price.
+
+#should contain a function `train()` that will create and store the model 
+#a function `predict()` that will take your preprocessed data as an input and return a price as output using your stored model.
