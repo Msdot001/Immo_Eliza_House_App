eb6b2898428f8241d7d05d632c7d87961a60afb1
Moshood Owolabi <98819100+Msdot001@users.noreply.github.com>
2022-05-03 16:51:22 +0200

    model development

--- Project/app.py
+++ Project/app.py
@@ -21,9 +21,9 @@ def predict():
     final_features = [np.array(init_features)]
 
     prediction = model.predict(final_features) # making prediction
-    prediction = list(map(lambda prediction :str(prediction), prediction.round(0)))
+    #prediction = list(map(lambda prediction :str(prediction), prediction.round(0)))
 
-    return render_template('index.html', prediction_text='Predicted Price: ${}'.format(prediction)) # rendering the predicted result
+    return render_template('index.html', prediction_text='Predicted Price: ${}'.format(int(prediction))) # rendering the predicted result
 
 if __name__ == "__main__":
     app.run(debug=True)
