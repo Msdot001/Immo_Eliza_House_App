d42cf4a9da76a48c6ddb0dc7a7e5bee6bfd39669
Moshood Owolabi <98819100+Msdot001@users.noreply.github.com>
2022-05-05 16:41:58 +0200

    deleting Project directory

--- Project/Preprocessing/cleaning_data.py
+++
@@ -1,4 +0,0 @@
-# It contain a function called `preprocess()` that will take a new house's data as input and return those data preprocessed as output.
-# If your data doesn't contain the required information, you should return an error to the user.
-
-
